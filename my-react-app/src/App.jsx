a293b91532614824bc2c4445cfb445d2e0a51377
grciastyl <grciastyl130@hotmail.com>
2025-06-17 17:57:58 -0600

    107 class 1

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/navBar';
+
+function App() { //App is a component and needs to be started with a capital letter
+
+  return (
+
+    <div className="App">
+      <Navbar /> 
+      <h1>Hello from REACT</h1>
+    </div>
+
+  );
+}
+
+export default App
